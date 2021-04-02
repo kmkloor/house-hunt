dd00c2407835dd3086b33b6b56ac4e5993ae172e
kate <kate.kloor@pattern.com>
2021-04-02 14:46:49 -0600

    Seed update

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -2,13 +2,13 @@ class StatusController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    
+      render json: Status.order(id: :desc)
   end
 
   def seed
-    Status.create_with(status: 'Considering', color: '#EEE8AA')
-    Status.create_with(status: 'Ready For Offer', color: '#90EE90')
-    Status.create_with(status: 'Offer Sent', color: '#4682B4')
+    Status.create(status: 'Considering', color: '#EEE8AA')
+    Status.create(status: 'Ready For Offer', color: '#90EE90')
+    Status.create(status: 'Offer Sent', color: '#4682B4')
   end
 
 end
