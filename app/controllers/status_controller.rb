efd2480716a763ef6520d786c77c0c021e8e8db0
kate <kate.kloor@pattern.com>
2021-04-02 14:36:25 -0600

    Seed status

---
+++ app/controllers/status_controller.rb
@@ -0,0 +1,14 @@
+class StatusController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    
+  end
+
+  def seed()
+    Status.create_with(status: 'Considering', color: '#EEE8AA')
+    Status.create_with(status: 'Ready For Offer', color: '#90EE90')
+    Status.create_with(status: 'Offer Sent', color: '#4682B4')
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'properties#show'
-  post 'welcome/getUrl/', to: 'welcome#getUrl'
+  get 'status/seed/', to: 'status#seed'
   get 'properties/add', to: 'properties#add'
   post 'properties/update', to: 'properties#update'
   get 'properties/show', to: 'properties#show'
