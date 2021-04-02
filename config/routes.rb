bc77d5a62232582c812f96d8e374485d06aa9a29
kate <kate.kloor@pattern.com>
2021-04-02 14:21:22 -0600

    Procfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'properties#show'
   post 'welcome/getUrl/', to: 'welcome#getUrl'
   get 'properties/add', to: 'properties#add'
   post 'properties/update', to: 'properties#update'
